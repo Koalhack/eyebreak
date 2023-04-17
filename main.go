5c0a9b437f61eb8289e0610ac9ef55523bb1b45c
KoalHack <alexandre.saner@gmail.com>
2023-04-17 20:08:06 +0200

    feat(main): add Key Shortcuts, keys indication and change styling of text

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"strings"
 	"time"
 
+	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-var intKeys = key.NewBinding(key.WithKeys("ctrl+c"))
+var (
+	boldStyle   = lipgloss.NewStyle().Bold(true)
+	italicStyle = lipgloss.NewStyle().Italic(true)
+)
 
 const (
 	padding  = 2
@@ -28,6 +33,17 @@ type model struct {
 
 	timer    timer.Model
 	progress progress.Model
+
+	keymap   keymap
+	help     help.Model
+	quitting bool
+}
+
+type keymap struct {
+	start key.Binding
+	stop  key.Binding
+	reset key.Binding
+	quit  key.Binding
 }
 
 func (m model) Init() tea.Cmd {
@@ -51,6 +67,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case timer.StartStopMsg:
 		var cmd tea.Cmd
 		m.timer, cmd = m.timer.Update(msg)
+
+		m.keymap.stop.SetEnabled(m.timer.Running())
+		m.keymap.start.SetEnabled(!m.timer.Running())
 		return m, cmd
 
 	case timer.TimeoutMsg:
@@ -80,18 +99,34 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// NOTE: Listen all Key Event
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, intKeys):
+		case key.Matches(msg, m.keymap.quit):
+			m.quitting = true
 			return m, tea.Quit
+		case key.Matches(msg, m.keymap.reset):
+			m.passed = 0
+			m.stateIndex = 0
+			m.timer.Timeout = m.durations[m.stateIndex]
+		case key.Matches(msg, m.keymap.start, m.keymap.stop):
+			return m, m.timer.Toggle()
 		}
 	}
 
 	return m, nil
 }
 
+func (m model) helpView() string {
+	return m.help.ShortHelpView([]key.Binding{
+		m.keymap.start,
+		m.keymap.stop,
+		m.keymap.reset,
+		m.keymap.quit,
+	})
+}
+
 func (m model) View() string {
 	pad := strings.Repeat(" ", padding)
-	s := "\n" + pad + m.state[m.stateIndex] + " - " + m.timer.View() +
-		"\n" + pad + m.progress.View() + "\n\n" + pad
+	s := "\n" + pad + italicStyle.Render(m.state[m.stateIndex]) + " - " + boldStyle.Render(m.timer.View()) +
+		"\n\n" + pad + m.progress.View() + "\n\n" + pad + m.helpView() + "\n"
 
 	if m.timer.Timedout() {
 		s = "All done!"
@@ -103,14 +138,35 @@ func (m model) View() string {
 func main() {
 	var durations = [2]time.Duration{time.Second * 30, time.Second * 20}
 	m := model{
-		state:      [2]string{"work", "look"},
+		state:      [2]string{"Work", "Look"},
 		stateIndex: 0,
 
 		durations: durations,
 
 		timer:    timer.NewWithInterval(durations[0], time.Second),
 		progress: progress.New(progress.WithDefaultGradient()),
+
+		keymap: keymap{
+			start: key.NewBinding(
+				key.WithKeys("s"),
+				key.WithHelp("s", "start"),
+			),
+			stop: key.NewBinding(
+				key.WithKeys("s"),
+				key.WithHelp("s", "stop"),
+			),
+			reset: key.NewBinding(
+				key.WithKeys("r"),
+				key.WithHelp("r", "reset"),
+			),
+			quit: key.NewBinding(
+				key.WithKeys("q", "ctrl+c"),
+				key.WithHelp("q", "quit"),
+			),
+		},
+		help: help.NewModel(),
 	}
+	m.keymap.start.SetEnabled(false)
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Uh oh, we encountered an error:", err)
